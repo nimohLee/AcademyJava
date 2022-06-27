7ef1108d6eec9f65fdb4d84056ebdcd11071b9c8
nimohLee <spakers38@gmail.com>
2022-06-27 15:35:57 +0900

    1일차

--- Var3.java
+++ Var3.java
@@ -5,15 +5,18 @@ class Var3{
 		Scanner scan = new Scanner(System.in);
 		System.out.println("몸무게를 입력하시오");
 		int wei = scan.nextInt();
-		System.out.println("키를 입력하시오(m)");
+		System.out.println("키를 입력하시오");
 		float hei = scan.nextFloat()/100;
-		
 		float bmi = wei/(hei*hei);
+	
+		System.out.println("당신의 BMI 지수는"+bmi+"입니다.");
 		
-		System.out.print("당신의 BMI 지수는"+bmi+"입니다.");
-		
-		
-		
+		if(bmi>=18.5&&bmi<=24.9)
+			System.out.println("정상체중입니다");
+		else if(bmi>=25)
+			System.out.println("비만입니다");
+
+
 		
 	}
 }
