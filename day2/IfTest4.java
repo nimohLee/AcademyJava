ffc13f9c9a2c34186a031b548cf14a81207257d8
nimohLee <spakers38@gmail.com>
2022-06-28 12:46:55 +0900

    day_2

---
+++ day2/IfTest4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class IfTest4{
+
+public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수 입력: ");
+		int point = sc.nextInt();
+		
+		if(point<=100&&point>=90)
+			System.out.print("결과 : A");
+		else if(point<=89 && point >= 80)
+			System.out.print("결과 : B");
+		else if(point<=79 && point >= 70)
+			System.out.print("결과 : C");
+		else if(point<=69 && point >= 60)
+			System.out.print("결과 : D");
+		else if(point<=59)
+			System.out.print("결과 : F");
+		else
+			System.out.print("잘못 입력되었습니다.");
+	
+}
+
+}
